191393c14e7e04e9c862eb2e1daadadf05f0aeab
Gereon <gereon.kremer@rwth-aachen.de>
2011-10-20 18:58:40 +0200

    it compiles, let's ship it\!

--- prog01_c++/dijkstra.cpp
+++ prog01_c++/dijkstra.cpp
@@ -6,23 +6,22 @@
 
 struct vert
 {
-	vertex v;
+	vertex::vertex_id v;
 	edge::weight_type dist;
 	
 	bool operator<(const vert& v) const
 	{
 		return this->dist < v.dist;
 	}
-	vert(vertex v, edge::weight_type dist): v(v), dist(dist) {}
+	vert(vertex::vertex_id v, edge::weight_type dist): v(v), dist(dist) {}
 	vert(const vert& v): v(v.v), dist(v.dist) {}
 };
 
 void dijkstra(graph& g, const vertex source, map<vertex::vertex_id, edge::weight_type> weights, map<vertex::vertex_id, vertex::vertex_id> pred)
 {
-	
 	priority_queue<vert> todo;
 	set<vertex::vertex_id> done;
-	vert src(source, 0);	
+	vert src(source.id, 0);	
 	todo.push(src);
 	done.insert(source.id);
 	pred[source.id] = 0;
@@ -37,19 +36,19 @@ void dijkstra(graph& g, const vertex source, map<vertex::vertex_id, edge::weight
 	{
 		vert cur = todo.top();
 		todo.pop();
-		vertex::vertex_id curid = cur.v.id;
+		vertex::vertex_id curid = cur.v;
 		edge::weight_type dst = weights[curid];
 		
 		typedef map<vertex::vertex_id, edge>::iterator mapit;
 		for (mapit it = g.edges[curid].begin(); it != g.edges[curid].end(); it++)
 		{
-			if (dst + it->second < weigths[it->first])
+			if (dst + it->second.weight < weights[it->first])
 			{
-				weights[it->first] = dst + it->second;
-				pred[it->first] = it->second;
-				if (! done.contains(it->first))
+				weights[it->first] = dst + it->second.weight;
+				pred[it->first] = curid;
+				if (done.find(it->first) == done.end())
 				{
-					vert next(it->first, weights[it->first]):
+					vert next(it->first, weights[it->first]);
 					todo.push(next);
 				}
 			}
