76607a442887ea039d261fb9f7ec7c9fe96b4e47
Gereon <gereon.kremer@rwth-aachen.de>
2011-10-24 19:03:49 +0200

    calculating a path between two random nodes...

--- prog01_lemon/main.cpp
+++ prog01_lemon/main.cpp
@@ -2,11 +2,13 @@
 #include <set>
 #include <sstream>
 #include <ctime>
+#include <cstdlib>
 
 #include "lemon/list_graph.h"
 #include "lemon/lgf_reader.h"
 
 #include "prim.h"
+#include "dijkstra.h"
 
 #define START_TIMER(x) clock_t x = clock();
 #define STOP_TIMER(x) cout << (clock() - x) / (CLOCKS_PER_SEC / 1000) << " ms" << endl;
@@ -16,6 +18,8 @@ using namespace lemon;
 
 int main()
 {
+	srand(time(NULL));
+	
 	for (int i=1; i<7; i++)
 	{
 		stringstream filename;
@@ -26,7 +30,7 @@ int main()
 		ListGraph::EdgeMap<int> weight(g);
 		graphReader(g, filename.str()).edgeMap("weight", weight).run();
 		
-		cout << "Calculation MST..." << endl;
+		cout << "Calculating MST..." << endl;
 		START_TIMER(tmr_mst)
 		Prim p(g, weight);
 		int w = p.prim();
@@ -36,15 +40,24 @@ int main()
 			cout << "\t" << g.id(g.u(*it)) << ", " << g.id(g.v(*it)) << endl;
 */
 		STOP_TIMER(tmr_mst)
+
+		int uid = rand() % countNodes(g);
+		int vid = rand() % countNodes(g);
+		int vi=vid, ui=uid;
+		ListGraph::Node u, v;
+		for (ListGraph::NodeIt n(g); n != INVALID; ++n, --ui, --vi)
+		{
+			if (ui == 0) u = n;
+			if (vi == 0) v = n;
+		}
+		cout << "Calculating distance between " << uid+1 << " and " << vid+1 << " using dijkstra..." << endl;
+		START_TIMER(tmr_dist)
+		Dijkstra dij(g, weight);
+		dij.dijkstra(u);
+		cout << "distance is " << (*dij.dist)[v] << endl;
+		STOP_TIMER(tmr_dist)
+		
 	}
 	
-/*
-	Dijkstra dij(g, weight);
-	dij.dijkstra(g.nodeFromId(3));
-	for (ListGraph::NodeIt n(g); n != INVALID; ++n)
-	{
-		cout << g.id(n) << " " << (*dij.dist)[n] << endl;
-	}
-*/
 	
 }
