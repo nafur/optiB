5376aa7c3d5b7ddbc8fc7a29edb54e213b2ac5dc
Gereon <gereon.kremer@rwth-aachen.de>
2011-10-25 20:58:09 +0200

    dijkstra works now, added a lengthy comment what is implemented (as it is not really dijkstra)

--- prog01_lemon/dijkstra.cpp
+++ prog01_lemon/dijkstra.cpp
@@ -1,6 +1,25 @@
 #include "dijkstra.h"
+#include "graph.h"
+
+#include <iostream>
 
 #include <queue>
+
+/*
+	Just a hint, if you look closer at this algorithm:
+	
+	This is not exactly dijkstra!
+	Dijkstra would always select the node with the smallest distance available, thus resulting in at most n iterations overall.
+	This selection however requires to store the node distance pairs in a fibbonaci heap to allow fast reorganizing.
+	
+	Instead, we just use a regular priority queue with the following mechanism:
+	If we find a shorter path to some node (i.e. we update dist and pred) we insert this node to the queue.
+	Thus, nodes might be contained multiple times (though less than n times) in the queue.
+	However, we pop always the nearest node (if the node has already been processed, we drop it).
+	At the end, when all nodes are processed, the remaining entries in the queue are dropped.
+	
+	The worst-case runtime is worse than one would expect from dijkstra, but in practice its good :-)
+*/
 
 void Dijkstra::dijkstra(const ListGraph::Node& source)
 {
@@ -12,14 +31,18 @@ void Dijkstra::dijkstra(const ListGraph::Node& source)
 	for (ListGraph::NodeIt n(this->g); n != INVALID; ++n)
 		(*this->dist)[n] = -1;
 	(*this->dist)[source] = 0;
+	(*this->pred)[source] = source;
 	
-	priority_queue<ListGraph::Node> todo;
-	todo.push(source);
+	priority_queue<cmpNode> todo;
+	todo.push(cmpNode(source, 0));
 	
 	while (! todo.empty())
 	{
-		ListGraph::Node cur = todo.top();
+		ListGraph::Node cur = todo.top().n;
+		int curdist = todo.top().w;
 		todo.pop();
+
+		if ((*this->dist)[cur] < curdist) continue;
 		
 		for (ListGraph::IncEdgeIt e(this->g, cur); e != INVALID; ++e)
 		{
@@ -32,7 +55,7 @@ void Dijkstra::dijkstra(const ListGraph::Node& source)
 			{
 				(*this->dist)[v] = (*this->dist)[cur] + w;
 				(*this->pred)[v] = cur;
-				todo.push(v);
+				todo.push(cmpNode(v, (*this->dist)[v]));
 			}	
 		}
 	}
