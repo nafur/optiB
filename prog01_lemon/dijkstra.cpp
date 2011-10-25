724548eab466a4e8a18ea2877460b1a16d04fc53
Gereon <gereon.kremer@rwth-aachen.de>
2011-10-25 21:24:15 +0200

    tiny optimizations, more comments

--- prog01_lemon/dijkstra.cpp
+++ prog01_lemon/dijkstra.cpp
@@ -23,38 +23,53 @@
 
 void Dijkstra::dijkstra(const ListGraph::Node& source)
 {
+	// create new result maps
 	if (this->dist != 0) delete this->dist;
 	this->dist = new ListGraph::NodeMap<int>(this->g);
 	if (this->pred != 0) delete this->pred;
 	this->pred = new ListGraph::NodeMap<ListGraph::Node>(this->g);
 	
+	// initialize distances
 	for (ListGraph::NodeIt n(this->g); n != INVALID; ++n)
 		(*this->dist)[n] = -1;
 	(*this->dist)[source] = 0;
 	(*this->pred)[source] = source;
 	
+	// start at source
 	priority_queue<cmpNode> todo;
 	todo.push(cmpNode(source, 0));
 	
+	// we count the number of processed nodes
+	// this should reduce the runtime, but is not necessary for this algorithm to work
+	int cnt = 0;
+	int max = countNodes(this->g);
+	
 	while (! todo.empty())
 	{
+		// already processed all nodes, we are done
+		if (cnt == max) break;
+		
+		// get nearest node from queue
 		ListGraph::Node cur = todo.top().n;
 		int curdist = todo.top().w;
 		todo.pop();
 
+		// we already processed this node (with a smaller distance), so drop it
 		if ((*this->dist)[cur] < curdist) continue;
+		cnt++; // we got another one
 		
 		for (ListGraph::IncEdgeIt e(this->g, cur); e != INVALID; ++e)
-		{
+		{ // go through all edges of this node
 			ListGraph::Node v = this->g.u(e);
 			if (v == cur) v = this->g.v(e);
 			
 			int w = this->weight[e];
 			
 			if (((*this->dist)[v] == -1) || ((*this->dist)[cur] + w < (*this->dist)[v]))
-			{
+			{ // other node of the edge has no or larger distance
 				(*this->dist)[v] = (*this->dist)[cur] + w;
 				(*this->pred)[v] = cur;
+				// insert it, possibly again, into the queue
 				todo.push(cmpNode(v, (*this->dist)[v]));
 			}	
 		}
