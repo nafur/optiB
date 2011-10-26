acc4ee0d8797710727c794b713a650b836fe5ff6
Gereon <gereon.kremer@rwth-aachen.de>
2011-10-26 20:15:57 +0200

    steiner should work now...

--- prog01_lemon/steiner.cpp
+++ prog01_lemon/steiner.cpp
@@ -14,12 +14,11 @@
 
 int Steiner::steiner(const set<ListGraph::Node> terminals)
 {
-	cerr << "start" << endl;
 	if (this->s != 0) delete this->s;
 	this->s = new ListGraph();
+	if (this->sweight != 0) delete this->sweight;
+	this->sweight = new ListGraph::EdgeMap<int>(*this->s);
 	
-	cerr << "dijkstras" << endl;
-
 	// perform dijkstra for every terminal
 	ListGraph::NodeMap<Dijkstra*> dijk(this->g);
 	for (set<ListGraph::Node>::iterator it = terminals.begin(); it != terminals.end(); ++it)
@@ -28,45 +27,64 @@ int Steiner::steiner(const set<ListGraph::Node> terminals)
 		dijk[*it]->dijkstra(*it);
 	}
 
-	cerr << "intermediate" << endl;
-	
 	// build intermediate graph
 	ListGraph intermediate;
 	ListGraph::EdgeMap<int> iweight(intermediate);
-	map<ListGraph::Node, ListGraph::Node> stog;
-	map<ListGraph::Node, ListGraph::Node> gtos;
+	map<ListGraph::Node, ListGraph::Node> imapper;
 	
 	for (set<ListGraph::Node>::iterator it = terminals.begin(); it != terminals.end(); ++it)
 	{
 		ListGraph::Node n = intermediate.addNode();
-		stog[n] = *it;
-		gtos[*it] = n;
+		imapper[n] = *it;
 	}
-	cerr << "inserting edges..." << endl;
 	for (ListGraph::NodeIt it1(intermediate); it1 != INVALID; ++it1)
 	{
 		ListGraph::NodeIt it2 = it1;
-		++it2;
-		for (; it2 != INVALID; ++it2)
+		for (++it2; it2 != INVALID; ++it2)
 		{
 			ListGraph::Edge e = intermediate.addEdge(it1, it2);
-			iweight[e] = (*dijk[stog[it1]]->dist)[stog[it2]];
+			iweight[e] = (*dijk[imapper[it1]]->dist)[imapper[it2]];
 		}
 	}
 	
-	dump(intermediate, iweight);
-	
+	// compute mst
 	Prim p(intermediate, iweight);
 	p.prim();
 	
-	
-	cerr << "cleanup" << endl;
+	// build final graph
+	map<ListGraph::Node, ListGraph::Node> smapper;
+	for (set<ListGraph::Edge>::iterator it = p.mst->begin(); it != p.mst->end(); ++it)
+	{ // for each edge in the mst
+		// add end nodes to graph
+		ListGraph::Node u = imapper[intermediate.u(*it)];
+		if (smapper.count(u) == 0) smapper[u] = this->s->addNode();
+		ListGraph::Node v = imapper[intermediate.v(*it)];
+		if (smapper.count(v) == 0) smapper[v] = this->s->addNode();
 
+		ListGraph::Node last = v;
+		ListGraph::Node cur = v;
+		do
+		{ // walk through path
+			cur = (*dijk[u]->pred)[cur];
+			if (smapper.count(cur) == 0) smapper[cur] = this->s->addNode();
+			// add edge to graph
+			ListGraph::Edge e = this->s->addEdge(smapper[last], smapper[cur]);
+			(*this->sweight)[e] = (*dijk[u]->dist)[last] - (*dijk[u]->dist)[cur];
+			last = cur;
+		}
+		while (cur != u);
+	}
+	
+	// compute overall weight
+	int overallw = 0;
+	for (ListGraph::EdgeIt e(*this->s); e != INVALID; ++e)
+	{
+		overallw += (*this->sweight)[e];
+	}
+	
 	// clean up dijkstras
 	for (ListGraph::NodeIt n(this->g); n != INVALID; ++n)
 		delete dijk[n];
 		
-	exit(0);
-	
-	return 0;
+	return overallw;
 }
--- prog01_lemon/steiner.h
+++ prog01_lemon/steiner.h
@@ -17,14 +17,17 @@ struct Steiner
 		Steiner(const ListGraph& graph, const ListGraph::EdgeMap<int>& weight):
 			g(graph),
 			weight(weight),
-			s(0)
+			s(0),
+			sweight(0)
 			{}
 		~Steiner()
 		{
 			if (this->s != 0) delete this->s;
+			if (this->sweight != 0) delete this->sweight;
 		}
 		
 		ListGraph* s;
+		ListGraph::EdgeMap<int>* sweight;
 		
 		int steiner(const set<ListGraph::Node> terminals);
 };
