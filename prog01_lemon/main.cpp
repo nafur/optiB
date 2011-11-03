380a928243ed238a9676730c459346cda8553018
Niklas Fischer <niklasfi@gmail.com>
2011-11-03 16:42:22 +0100

    made the notes compile

--- prog01_lemon/main.cpp
+++ prog01_lemon/main.cpp
@@ -23,37 +23,33 @@ int main()
 	
 	cout << "***** Aufgabe 1 *****" << endl;
 	cout << "Kruskal's Algorithm " << endl;
-	cout << "
-We first sort all edges by inserting them into a priority queue, resulting in a complexity of O(m * log(m)).
-For each edge, we then check if it connects two different node sets. This is the case exactly n times.
-If this is the case, we merge two vertex sets by iterating over all vertices.
-Otherwise, we continue with the next edge.
-
-Overall, this results in a complexity of O(m * log(m) + m + n * n) = O(n*n + m*log(m)).
-Using a more sophisticated set datastructure that is optimized to perform union operations might result in a slightly better complexity.
-" << endl;
+	cout << "We first sort all edges by inserting them into a priority queue, resulting in a complexity of O(m * log(m))." << endl
+	<< "For each edge, we then check if it connects two different node sets. This is the case exactly n times." << endl
+	<< "If this is the case, we merge two vertex sets by iterating over all vertices." << endl
+	<< "Otherwise, we continue with the next edge." << endl
+	<< endl
+	<< "Overall, this results in a complexity of O(m * log(m) + m + n * n) = O(n*n + m*log(m))." << endl
+	<< endl << "Using a more sophisticated set datastructure that is optimized to perform union operations might result in a slightly better complexity."
+	<< endl;
 	
 	cout << endl << "***** Aufgabe 2 *****" << endl;
 	cout << "Dijkstra's Algorithm " << endl;
-	cout << "
-This is not the most efficient dijkstra implementation, as a fibbonaci heap is necessary for this.
-As we use a simple priority queue, we have a slightly higher complexity.
-Using a traditional priority queue, we have to insert a node with it's current distance to the queue, every time this node is updated.
-Thereby, the queue can contain each node multiple times.
-
-We have the following complexity estimation:
-Over the run of the algorithm, the queue can contain at most m nodes, m being the number of edges.
-For n of these nodes, being the first occurences of the specific node, we iterate over all (at most n) incident edges and update the distances of the incident nodes.
-An update of a distance will then produce an insertion of a node to the priority queue, resulting in a log(m) overhead for reorganizing the heap underneath.
-For all other nodes, we will notice that the distance they had when being inserted is not the distance they have in this moment. If this is the case, they are ignored.
-
-Thus, we have a worst case complexity of O(m * log(m) + m).
-As log(m) is at most log(n*n) = log(n) * 2, the complexity is O(m * log(n))
-
-Optimal dijkstra results in a worst case complexity of O(n * log(n) + m).
-As our graphs usually have only as many edges as they have nodes, our implementation will not be much slower.
-If the triangle inequality holds in the graph, our implementation will be as fast as the optimal one, as no nodes are inserted a second time.
-" << endl;
+	cout << "This is not the most efficient dijkstra implementation, as a fibbonaci heap is necessary for this." << endl
+	<< "As we use a simple priority queue, we have a slightly higher complexity." << endl
+	<< "Using a traditional priority queue, we have to insert a node with it's current distance to the queue, every time this node is updated." << endl
+	<< "Thereby, the queue can contain each node multiple times." << endl
+	<< endl
+	<< "We have the following complexity estimation:" << endl
+	<< "Over the run of the algorithm, the queue can contain at most m nodes, m being the number of edges." << endl
+	<< "For n of these nodes, being the first occurences of the specific node, we iterate over all (at most n) incident edges and update the distances of the incident nodes." << endl
+	<< "An update of a distance will then produce an insertion of a node to the priority queue, resulting in a log(m) overhead for reorganizing the heap underneath." << endl
+	<< "For all other nodes, we will notice that the distance they had when being inserted is not the distance they have in this moment. If this is the case, they are ignored." << endl
+	<< endl
+	<< "Thus, we have a worst case complexity of O(m * log(m) + m)." << endl
+	<< "As log(m) is at most log(n*n) = log(n) * 2, the complexity is O(m * log(n))" << endl
+	<< "Optimal dijkstra results in a worst case complexity of O(n * log(n) + m).\n"
+	<< "As our graphs usually have only as many edges as they have nodes, our implementation will not be much slower.\n"
+	<< "If the triangle inequality holds in the graph, our implementation will be as fast as the optimal one, as no nodes are inserted a second time." << endl;
 	
 	cout << endl << "***** Aufgabe 3 *****" << endl;
 	cout << "Algorithm to efficiently find a good approximation for a steiner tree" << endl;
@@ -88,7 +84,7 @@ If the triangle inequality holds in the graph, our implementation will be as fas
 		int w = k.kruskal();
 		
 		cout << " overall weight is " << w << endl;
-		cout << "the output of the edges is disabled. Please enable it in the code ad line " << (__LINE__+2) << endl;
+		cout << "the output of the edges is disabled. Please enable it in the code at line " << (__LINE__+2) << endl << endl;
 
 /*		cout << "MST contains the following edges" << endl;
 		for (set<ListGraph::Edge>::iterator it = p.mst->begin(); it != p.mst->end(); ++it)
@@ -128,9 +124,9 @@ If the triangle inequality holds in the graph, our implementation will be as fas
 			{
 				cout << "Printing steiner tree for first example." << endl;
 				cout << "note that the given ids are not garantued to match those in the file..." << endl;
-				for (ListGraph::EdgeIt e(steiner->s); e != INVALID; ++e)
+				for (ListGraph::EdgeIt e(*steiner.s); e != INVALID; ++e)
 				{
-					cout << "\t" << steiner->s.id(e.u)+1 << " -- " << steiner->s.id(e.v)+1 << endl;
+					cout << "\t" << steiner.s->id(steiner.s->u(e))+1 << " -- " << steiner.s->id(steiner.s->v(e))+1 << endl;
 				}
 			}
 		}
