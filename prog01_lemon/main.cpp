836efb3992d2db68382d559b0cf10358866665a9
gereon <gereon@fsmpi.rwth-aachen.de>
2011-11-03 16:23:16 +0100

    patched main.cpp to match tasks

--- prog01_lemon/main.cpp
+++ prog01_lemon/main.cpp
@@ -23,16 +23,44 @@ int main()
 	
 	cout << "***** Aufgabe 1 *****" << endl;
 	cout << "Kruskal's Algorithm " << endl;
+	cout << "
+We first sort all edges by inserting them into a priority queue, resulting in a complexity of O(m * log(m)).
+For each edge, we then check if it connects two different node sets. This is the case exactly n times.
+If this is the case, we merge two vertex sets by iterating over all vertices.
+Otherwise, we continue with the next edge.
+
+Overall, this results in a complexity of O(m * log(m) + m + n * n) = O(n*n + m*log(m)).
+Using a more sophisticated set datastructure that is optimized to perform union operations might result in a slightly better complexity.
+" << endl;
 	
 	cout << endl << "***** Aufgabe 2 *****" << endl;
 	cout << "Dijkstra's Algorithm " << endl;
+	cout << "
+This is not the most efficient dijkstra implementation, as a fibbonaci heap is necessary for this.
+As we use a simple priority queue, we have a slightly higher complexity.
+Using a traditional priority queue, we have to insert a node with it's current distance to the queue, every time this node is updated.
+Thereby, the queue can contain each node multiple times.
+
+We have the following complexity estimation:
+Over the run of the algorithm, the queue can contain at most m nodes, m being the number of edges.
+For n of these nodes, being the first occurences of the specific node, we iterate over all (at most n) incident edges and update the distances of the incident nodes.
+An update of a distance will then produce an insertion of a node to the priority queue, resulting in a log(m) overhead for reorganizing the heap underneath.
+For all other nodes, we will notice that the distance they had when being inserted is not the distance they have in this moment. If this is the case, they are ignored.
+
+Thus, we have a worst case complexity of O(m * log(m) + m).
+As log(m) is at most log(n*n) = log(n) * 2, the complexity is O(m * log(n))
+
+Optimal dijkstra results in a worst case complexity of O(n * log(n) + m).
+As our graphs usually have only as many edges as they have nodes, our implementation will not be much slower.
+If the triangle inequality holds in the graph, our implementation will be as fast as the optimal one, as no nodes are inserted a second time.
+" << endl;
 	
 	cout << endl << "***** Aufgabe 3 *****" << endl;
 	cout << "Algorithm to efficiently find a good approximation for a steiner tree" << endl;
 	cout << "Idea:" << endl;
 	cout << "\tRun Dijkstra to find all shortest paths from every terminal node to all other terminal nodes" << endl;
 	cout << "\tCreate an intermediate graph containing all terminal nodes" << endl;
-	cout << "\tFor each pair of nodes insert an edge with weight being the length of shortest path between those nodes" << endl;
+	cout << "\tFor each pair of nodes insert an edge with weight being the length of shortest path between those nodes into the intermediate graph" << endl;
 	cout << "\tCalculate a MST on this intermediate graph" << endl;
 	cout << "\tReplace every edge in the intermediate graph with the corresponding shortest path" << endl;
 	cout << "The result is a connected graph containing at least all terminal nodes" << endl;
@@ -42,9 +70,9 @@ int main()
 	cout << "A MST in the intermediate graph is (a,b), (a,d), (b,c) with an overall weight of 5" << endl;
 	cout << "However, an optimal steiner tree would be (a,e), (b,e), (c,e), (d,e) with an overall weight of 4" << endl;
 
-	cout << endl << "***** Aufgabe 3 *****" << endl;
+	cout << endl << "***** Aufgabe 4 *****" << endl;
 	
-	
+	cout << endl << "*** 4 a) ***" << endl;
 	START_TIMER(tmr_mst)
 	for (int i=1; i<7; i++)
 	{
@@ -56,12 +84,11 @@ int main()
 		graphReader(g, filename.str()).edgeMap("weight", weight).run();
 		
 		cout << "Calculating MST for graph " << i << "...";
-//		Prim p(g, weight);
-//		int w = p.prim();
 		Kruskal k(g, weight);
 		int w = k.kruskal();
 		
 		cout << " overall weight is " << w << endl;
+		cout << "the output of the edges is disabled. Please enable it in the code ad line " << (__LINE__+2) << endl;
 
 /*		cout << "MST contains the following edges" << endl;
 		for (set<ListGraph::Edge>::iterator it = p.mst->begin(); it != p.mst->end(); ++it)
@@ -70,6 +97,7 @@ int main()
 	}
 	STOP_TIMER(tmr_mst)
 
+	cout << endl << "*** 4 b) and c) ***" << endl;
 	for (int i=1; i<7; i++)
 	{
 		stringstream filename;
@@ -96,6 +124,15 @@ int main()
 			cout << "Upper bound for terminals " << term << ": " << steinerw << endl;
 			cout << "steiner: ";
 			STOP_TIMER(tmr_steiner)
+			if (i == 1)
+			{
+				cout << "Printing steiner tree for first example." << endl;
+				cout << "note that the given ids are not garantued to match those in the file..." << endl;
+				for (ListGraph::EdgeIt e(steiner->s); e != INVALID; ++e)
+				{
+					cout << "\t" << steiner->s.id(e.u)+1 << " -- " << steiner->s.id(e.v)+1 << endl;
+				}
+			}
 		}
 	}
 }
