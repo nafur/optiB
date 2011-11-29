14852653c27f6dddd7787bf905276d71d522aac0
Gereon <gereon.kremer@rwth-aachen.de>
2011-11-29 14:56:54 +0100

    initialized main for exercise 2

---
+++ prog/main_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <cstdlib>
+
+#include <lemon/list_graph.h>
+#include <lemon/lgf_reader.h>
+
+#include "debug.h"
+#include "graph.h"
+#include "prim.h"
+#include "kruskal.h"
+#include "dijkstra.h"
+#include "steiner.h"
+
+using namespace std;
+using namespace lemon;
+
+int main()
+{
+	srand(time(NULL));
+	
+}
