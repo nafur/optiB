40274a35624deeb233d0bbb896569537d216455a
Gereon <gereon.kremer@rwth-aachen.de>
2011-11-30 20:56:34 +0100

    put bipartite tests into main

--- prog/main_2.cpp
+++ prog/main_2.cpp
@@ -7,11 +7,7 @@
 #include <lemon/lgf_reader.h>
 
 #include "debug.h"
-#include "graph.h"
-#include "prim.h"
-#include "kruskal.h"
-#include "dijkstra.h"
-#include "steiner.h"
+#include "bipartite.h"
 
 using namespace std;
 using namespace lemon;
@@ -19,5 +15,28 @@ using namespace lemon;
 int main()
 {
 	srand(time(NULL));
-	
+
+	cout << endl << "*** 2) ***" << endl;
+	for (int i=1; i<5; i++)
+	{
+		stringstream filename;
+		filename << "data_2/graph" << i << ".lgf";
+		
+		ListGraph g;
+		ListGraph::EdgeMap<int> weight(g);
+		graphReader(g, filename.str()).edgeMap("weight", weight).run();	
+		
+		cout << "Checking if graph " << i << " is bipartite..." << endl;
+		
+		Bipartite b(g);
+		if (b.bipartite()) cout << "yes, it is!" << endl;
+		else cout << "no, its not..." << endl;
+		
+		cout << "Dumping classes: " << endl;
+		for (ListGraph::NodeIt n(g); n != INVALID; ++n)
+		{
+			cout << g.id(n)+1 << " " << (*b.classes)[n] << ", ";
+		}
+		cout << endl;
+	}
 }
