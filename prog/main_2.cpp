571082aa26b9c3f7bd29a323968bf9dee58dcc37
gereon <gereon@fsmpi.rwth-aachen.de>
2011-12-01 17:04:11 +0100

    added all res allocation instances

--- prog/main_2.cpp
+++ prog/main_2.cpp
@@ -18,6 +18,7 @@ int main()
 	srand(time(NULL));
 
 	cout << endl << "*** 2) ***" << endl;
+	START_TIMER(tmr_bipartite)
 	for (int i=1; i<5; i++)
 	{
 		stringstream filename;
@@ -27,23 +28,31 @@ int main()
 		ListGraph::EdgeMap<int> weight(g);
 		graphReader(g, filename.str()).edgeMap("weight", weight).run();	
 		
-		cout << "Checking if graph " << i << " is bipartite..." << endl;
+		cout << "Checking if graph " << i << " is bipartite... ";
 		
 		Bipartite b(g);
 		if (b.bipartite()) cout << "yes, it is!" << endl;
-		else cout << "no, its not..." << endl;
+		else cout << "no, it's not!" << endl;
 		
+		/*
 		cout << "Dumping classes: " << endl;
 		for (ListGraph::NodeIt n(g); n != INVALID; ++n)
 		{
 			cout << g.id(n)+1 << " " << (*b.classes)[n] << ", ";
 		}
 		cout << endl;
+		*/
 	}
+	STOP_TIMER(tmr_bipartite)
 	
 	cout << endl << "*** 3) ***" << endl;
-	
-	stringstream filename;
-	filename << "data_2/RescAllo1.txt";
-	Allocation a(filename.str(), 10);
+	START_TIMER(tmr_rescallo)
+	for (int i=1; i<4; i++)
+	{
+		stringstream filename;
+		filename << "data_2/RescAllo" << i << ".txt";
+		Allocation a(filename.str(), 10);
+		cout << "Maximum profit for instance " << i << ": " << a.allocate() << endl;
+	}
+	STOP_TIMER(tmr_rescallo)
 }
