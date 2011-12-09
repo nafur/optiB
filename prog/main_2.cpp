87c1adae494c0325431c485a00baa738d2e55a2e
Gereon <gereon.kremer@rwth-aachen.de>
2011-12-10 00:13:12 +0100

    added lots of explanations for task 1-3 added task 4

--- prog/main_2.cpp
+++ prog/main_2.cpp
@@ -11,6 +11,7 @@
 #include "bipartite.h"
 #include "allocation.h"
 #include "tsp.h"
+#include "tsp_relax.h"
 
 using namespace std;
 using namespace lemon;
@@ -18,17 +19,69 @@ using namespace lemon;
 int main()
 {
 	srand(time(NULL));
-
+	
 	cout << endl << "*** 1) ***" << endl;
+	cout << endl;
+	cout << "A minimal network that is 1-redundant (still connected if one edge fails)" << endl;
+	cout << "is the union of connected cycles. If the graph is metric, the optimal solution" << endl;
+	cout << "will be a solution for the TSP." << endl;
+	cout << "(Actually, we don't know if the above holds, but TSP should at least be a good" << endl;
+	cout << "approximation...)" << endl;
+	cout << endl;
+	cout << "Therefore, we implement a heuristic to find a hamiltonian cycle that is as" << endl;
+	cout << "short as possible. To be exact, we implement the Christofides-Heuristic." << endl;
+	cout << "The idea of this one is as follows:" << endl;
+	cout << "\tTake a MST of the graph" << endl;
+	cout << "\tTake all nodes, that have odd degree within the MST" << endl;
+	cout << "\tCompute a matching on these nodes" << endl;
+	cout << "\tAdd all matching edges to the MST" << endl;
+	cout << "\tThe extended MST now has an eulerian cycle, because all nodes have even degree" << endl;
+	cout << "\tIterate over some eulerian cycle" << endl;
+	cout << "\t\tIf next node is visited for the first time (i.e. is new)" << endl;
+	cout << "\t\t\tadd edge from last new node to the next one to result" << endl;
+	cout << "\t\tIf next node has already been visited" << endl;
+	cout << "\t\t\tskip node" << endl;
+	cout << endl;
+	cout << "This approximation is a 1.5-Approximation, because" << endl;
+	cout << "\tThe MST is smaller than the TSP, as the TSP without some edge is a spanning tree" << endl;
+	cout << "\tThe matching has at least half the weight of a MST" << endl;
+	cout << "\tThe refactoring of the eulerian cycle only decreases the weight" << endl;
+	cout << endl;
+	
 	ListGraph g;
 	ListGraph::EdgeMap<int> weight(g);
 	readMatrix("data_2/Deutschland.txt", g, weight);
 	TSP tsp(g, weight);
 	int w = tsp.tsp();
-	cout << "TSP weight: " << w << endl;
-	
+	cout << "result has weight " << w << endl;	
 
 	cout << endl << "*** 2) ***" << endl;
+	cout << endl;
+	cout << "Checking if a graph is bipartite actually is the same as 2-coloring:" << endl;
+	cout << "If you can color the nodes with two colors, such that every two nodes" << endl;
+	cout << "that are connected do not have the same color, you are done." << endl;
+	cout << "If cou can not do that, there must be a cycle of odd length, which" << endl;
+	cout << "violates the assumption that the graph is bipartite." << endl;
+	cout << endl;
+	cout << "So what we do is exactly this: we color the graph with colors 1 and 2." << endl;
+	cout << "We start with all nodes having no color." << endl;
+	cout << "For all nodes n" << endl;
+	cout << "\tif n is colored, skip" << endl;
+	cout << "\tcolor n with 1" << endl;
+	cout << "\tdo DFS(n, 1)" << endl;
+	cout << endl;
+	cout << "DFS(n, i):" << endl;
+	cout << "\tn has color i, let other color be j" << endl;
+	cout << "\tFor all neighbours m of n:" << endl;
+	cout << "\t\tif m is not colored:" << endl;
+	cout << "\t\t\tcolor m with j" << endl;
+	cout << "\t\t\tdo DFS(m, j)" << endl;
+	cout << "\t\tif m has color i:" << endl;
+	cout << "\t\t\tm and n have same color, return error" << endl;
+	cout << endl;
+	cout << "If this methods succeeds, the graph is bipartite, otherwise it's not." << endl;
+	cout << endl;
+	
 	START_TIMER(tmr_bipartite)
 	for (int i=1; i<5; i++)
 	{
@@ -42,21 +95,38 @@ int main()
 		cout << "Checking if graph " << i << " is bipartite... ";
 		
 		Bipartite b(g);
-		if (b.bipartite()) cout << "yes, it is!" << endl;
-		else cout << "no, it's not!" << endl;
-		
-		/*
-		cout << "Dumping classes: " << endl;
-		for (ListGraph::NodeIt n(g); n != INVALID; ++n)
+		if (b.bipartite())
 		{
-			cout << g.id(n)+1 << " " << (*b.classes)[n] << ", ";
+			cout << "yes, it is!" << endl;
+			cout << "printing color classes is disabled. enable it in the code at line " << (__LINE__+1) << endl << endl;
+			/* //print color classes
+			for (ListGraph::NodeIt n(g); n != INVALID; ++n)
+				cout << "Node " << g.id(n)+1 << " has color " << (*b.classes)[n] << endl;
+			*/
 		}
-		cout << endl;
-		*/
+		else cout << "no, it's not!" << endl;
 	}
 	STOP_TIMER(tmr_bipartite)
 	
 	cout << endl << "*** 3) ***" << endl;
+	cout << endl;
+	cout << "Although the resource allocation problem looks np complete at first sight," << endl;
+	cout << "it is possible to solve it in polynomial time." << endl;
+	cout << "We are using a dynamic programming scheme:" << endl;
+	cout << "\tf is int[machines][plants]" << endl;
+	cout << "\tf[i][j] is the maximum profit when" << endl;
+	cout << "\t\ti machines can be used" << endl;
+	cout << "\t\tplants j, j+1, ... can be used" << endl;
+	cout << endl;
+	cout << "Filling f is quite simple. Just start at the last plant. For this one" << endl;
+	cout << "the corresponding column of the given table can be copied." << endl;
+	cout << "For every other plant, you just have to check every combination of" << endl;
+	cout << "distributing the available machines to the current and the next plant." << endl;
+	cout << "The overall result will be f[machines][0]" << endl;
+	cout << endl;
+	cout << "As the number of distributions to split up k machines on two plants" << endl;
+	cout << "is only k+1, the overall complexity is O(machines * machines * plants)." << endl;
+	
 	START_TIMER(tmr_rescallo)
 	for (int i=1; i<4; i++)
 	{
@@ -66,4 +136,26 @@ int main()
 		cout << "Maximum profit for instance " << i << ": " << a.allocate() << endl;
 	}
 	STOP_TIMER(tmr_rescallo)
+	
+	cout << endl << "*** 4) ***" << endl;
+	START_TIMER(tmr_tsp_relax)
+	int files[] = {15, 75, 150};
+	for (int i=0; i<3; i++)
+	{
+		stringstream filename;
+		filename << "data_2/TSP" << files[i] << ".data";
+
+		ListGraph g;
+		ListGraph::EdgeMap<int> weight(g);
+		readMatrix(filename.str(), g, weight);
+		
+		TSPRelaxation tsp(g, weight);
+		cout << "1st lower bound for TSP" << files[i] << " is " << tsp.mstPlusOne() << endl;
+		int val = tsp.mstOnSubgraph();
+		int num = 0;
+		for (; num * num < countNodes(g); num++)
+			val = min(val, tsp.mstOnSubgraph());
+		cout << "2nd lower bound for TSP" << files[i] << " is " << val << " from " << num << " tries" << endl;
+	}
+	STOP_TIMER(tmr_tsp_relax)
 }
